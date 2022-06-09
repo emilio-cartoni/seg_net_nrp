7cf6a030f9d5ce78c5f5508118aebedf6e96575f
albornet <alban.bornet@gmail.ch>
2022-06-09 15:37:48 +0200

    updated documentation and dataloaders now defined in init

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from src.dataset_fn_handover import handover_dl
 flags.DEFINE_boolean('debug', False, 'Debug mode')
 flags.DEFINE_boolean('load_model', False, 'Load model last checkpoint (if available)')
 flags.DEFINE_integer('batch_size', 4, 'Batch size')
-flags.DEFINE_integer('num_epochs', 20, 'Number of epochs to train')
+flags.DEFINE_integer('num_epochs', 50, 'Number of epochs to train')
 flags.DEFINE_integer('num_workers', 4, 'Number of workers for dataloader')
 flags.DEFINE_integer('num_classes', 5, 'Number of classes to segment')
 flags.DEFINE_integer('num_frames', 10, 'Number of frames in each sequence')
 flags.DEFINE_integer('num_versions', 1, 'Number of versions to train for each model')
+flags.DEFINE_string('log_dir', 'logs', 'Path to log directory (e.g., for checkpoints)')
 flags.DEFINE_string('data_dir', '/mnt/d/DL/datasets/nrp/handover', 'Data directory')
 flags.DEFINE_string('scheduler_type', 'onecycle', 'Scheduler used to update learning rate')
 flags.DEFINE_float('lr', 5e-4, 'Learning rate')
@@ -31,6 +32,7 @@ class PLPredNet(pl.LightningModule):
     ''' Python lightning version of PredNet '''
     def __init__(self, device):
         ''' Initialize the model
+            Dataloaders also defined here to compute num_batches
         
         Args:
         -----
@@ -44,6 +46,7 @@ class PLPredNet(pl.LightningModule):
             Whether to include axonal delays or not
         pred_loss: bool
             Whether or not to minimize prediction error
+
         '''
         super().__init__()
         # Select fair number of channels for each combination
@@ -60,6 +63,23 @@ class PLPredNet(pl.LightningModule):
                              seg_layers=seg_layers,
                              bu_channels=(3,) + channels[:-1],
                              td_channels=channels)
+        self.train_dl = handover_dl(mode='train',
+                                    data_dir=FLAGS.data_dir,
+                                    batch_size=FLAGS.batch_size,
+                                    num_frames=FLAGS.num_frames,
+                                    num_classes=FLAGS.num_classes,
+                                    shuffle=True,
+                                    drop_last=True,
+                                    num_workers=FLAGS.num_workers)
+        self.val_dl = handover_dl(mode='valid',
+                                  data_dir=FLAGS.data_dir,
+                                  batch_size=FLAGS.batch_size,
+                                  num_frames=FLAGS.num_frames,
+                                  num_classes=FLAGS.num_classes,
+                                  shuffle=False,
+                                  drop_last=True,
+                                  num_workers=FLAGS.num_workers)
+        self.num_training_batches = len(self.train_dl)
         self.loss_fn = loss_fn
 
     def prepare_data(self):
@@ -83,6 +103,7 @@ class PLPredNet(pl.LightningModule):
             Sequence of n_frames image predictions
         S_seq: list of torch.Tensor of shape (b, c, h*, w*)
             Sequence of n_frames segmentation predictions
+
         '''
         E_seq, P_seq, S_seq = [], [], []
         TA = self.model.axon_delay
@@ -106,6 +127,7 @@ class PLPredNet(pl.LightningModule):
         --------
         dict:
             Dictionary containing the loss
+
         '''
         E_seq, _, S_seq = self.forward(batch['samples'])
         S_seq_true = batch['labels']
@@ -121,24 +143,22 @@ class PLPredNet(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         ''' Validation step of the model
             
-            Args:
-            -----
-            batch: dict
-                Dictionary containing the batch data
-            batch_idx: int
-                Index of the batch being processed
+        Args:
+        -----
+        batch: dict
+            Dictionary containing the batch data
+        batch_idx: int
+            Index of the batch being processed
 
-            Returns:
-            --------
-            dict:
-                Dictionary containing the loss and prediction
+        Returns:
+        --------
+        dict:
+            Dictionary containing the loss and prediction
+
         '''
         E_seq, P_seq, S_seq = self.forward(batch['samples'])
         P_seq_true, S_seq_true = batch['samples'], batch['labels']
-        loss = self.loss_fn(E_seq,
-                            S_seq,
-                            S_seq_true,
-                            val_flag=True)
+        loss = self.loss_fn(E_seq, S_seq, S_seq_true, val_flag=True)
         self.log('valid_loss', loss.cpu())
         sequences = {'P_seq': torch.stack(P_seq, dim=-1),
                      'P_seq_true': P_seq_true,
@@ -149,10 +169,11 @@ class PLPredNet(pl.LightningModule):
     def validation_epoch_end(self, outputs):
         ''' Instructions run at the end of every validation epoch
             
-            Args:
-            -----
-            outputs: list of dict
-                List of dictionaries containing the loss and prediction
+        Args:
+        -----
+        outputs: list of dict
+            List of dictionaries containing the loss and prediction
+
         '''
         sequences = outputs[0]['sequences']
         writer = self.logger.experiment
@@ -163,27 +184,11 @@ class PLPredNet(pl.LightningModule):
 
     def train_dataloader(self):
         ''' Training dataloader '''
-        dataloader = handover_dl(mode='train',
-                                 data_dir=FLAGS.data_dir,
-                                 batch_size=FLAGS.batch_size,
-                                 num_frames=FLAGS.num_frames,
-                                 num_classes=FLAGS.num_classes,
-                                 shuffle=True,
-                                 drop_last=True,
-                                 num_workers=FLAGS.num_workers)
-        return dataloader
+        return self.train_dl
 
     def val_dataloader(self):
         ''' Validation dataloader '''
-        dataloader = handover_dl(mode='valid',
-                                 data_dir=FLAGS.data_dir,
-                                 batch_size=FLAGS.batch_size,
-                                 num_frames=FLAGS.num_frames,
-                                 num_classes=FLAGS.num_classes,
-                                 shuffle=False,
-                                 drop_last=True,
-                                 num_workers=FLAGS.num_workers)
-        return dataloader
+        return self.val_dl
     
     def configure_optimizers(self):
         ''' Define the optimizer and  the learning rate scheduler '''
@@ -191,8 +196,8 @@ class PLPredNet(pl.LightningModule):
         scheduler = select_scheduler(optimizer,
                                      scheduler_type=FLAGS.scheduler_type,
                                      lr=FLAGS.lr,
-                                     n_batches=49,  # self.trainer.num_training_batches,
-                                     epochs=FLAGS.num_epochs)
+                                     num_epochs=FLAGS.num_epochs,
+                                     num_batches=self.num_training_batches)
         return [optimizer], [scheduler]
     
 
@@ -215,39 +220,38 @@ def define_fair_channels():
     return channels
 
 
-def generate_ckpt_path(log_dir, model_name, model_version):
+def generate_ckpt_path(model_name, model_version):
     ''' Generate the path to a potentially existing checkpoint.
         Note that, in case a ckpt is found but load_model=False,
         earlier checkpoints are added until the lasest one is
         reached, point from which the latest one is overwritten.
 
-        Args:
-        -----
-        log_dir: str
-            Path to the log directory
-        model_name: str
-            Name of the model
-        model_version: int
-            Version of the model
+    Args:
+    -----
+    model_name: str
+        Name of the model
+    model_version: int
+        Version of the model
 
-        Returns:
-        --------
-        ckpt_path: str or None
-            Path to checkpoint file (None if no checkpoint found)
+    Returns:
+    --------
+    ckpt_path: str or None
+        Path to checkpoint file (None if no checkpoint found)
+
     '''
     if FLAGS.load_model:
         try:
-            ckpt_dir = os.path.join(log_dir,
+            ckpt_dir = os.path.join(FLAGS.log_dir,
                                     model_name,
                                     f'version_{model_version}',
                                     'checkpoints')
             ckpt_path = os.path.join(ckpt_dir,
                                      os.listdir(ckpt_dir)[-1])
         except FileNotFoundError:
-            os.makedirs(log_dir, exist_ok=True)
+            os.makedirs(FLAGS.log_dir, exist_ok=True)
             ckpt_path = None
     else:
-        os.makedirs(log_dir, exist_ok=True)
+        os.makedirs(FLAGS.log_dir, exist_ok=True)
         ckpt_path = None
     return ckpt_path
 
@@ -255,26 +259,26 @@ def generate_ckpt_path(log_dir, model_name, model_version):
 def train_one_net(model_version):
     ''' Train one PredNet model using the pytorch-lightning framework
         
-        Args:
-        -----
-        model_version: str
-            Version of the model, to index different runs
+    Args:
+    -----
+    model_version: str
+        Version of the model, to index different runs
+        
     '''
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    log_dir = 'logs'
     model_name = f'Prednet_L-{FLAGS.n_layers}_R-{FLAGS.rnn_type}'\
                + f'_A-{FLAGS.axon_delay}_P-{FLAGS.pred_loss}'
     model = PLPredNet(device)
-    trainer = pl.Trainer(default_root_dir=log_dir,
+    trainer = pl.Trainer(default_root_dir=FLAGS.log_dir,
                          gpus=(1 if device=='cuda' else 0),
                          max_epochs=FLAGS.num_epochs,
                          fast_dev_run=FLAGS.debug,
                          log_every_n_steps=5,
                          logger=pl.loggers.TensorBoardLogger(
-                             save_dir=log_dir,
+                             save_dir=FLAGS.log_dir,
                              name=model_name,
                              version=model_version))
-    ckpt_path = generate_ckpt_path(log_dir, model_name, model_version)
+    ckpt_path = generate_ckpt_path(model_name, model_version)
     trainer.fit(model, ckpt_path=ckpt_path)
 
 
