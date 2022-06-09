1fae9b0bdd82afd612e4c7c2cb14d2171a5a7abb
albornet <alban.bornet@gmail.ch>
2022-06-09 15:36:17 +0200

    run.sh now asks whether to delete logs

--- run.sh
+++ run.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
+echo 'Reset checkpoint data (delete ./logs folder)? (y/n)'
+read delete_logs
+if [ $delete_logs == 'y' || $delte_logs == 'Y' ]; then
+    echo 'Deleting checkpoint data.'
+    rm -rf ./logs
+elif [ $delete_logs == 'y' || $delte_logs == 'Y' ]; then
+    echo 'No checkoint data will be deleted.'
+else 
+    echo 'Invalid input. Aborting.'
+    exit
+fi
+
 # declare -a pred_loss_array=('1' '0')
 # declare -a n_layers_array=('4' '3')
 # declare -a rnn_type_array=('hgru' 'conv' 'lstm')
 # declare -a axon_delay_array=('1' '0')
-
 # for pred_loss in ${pred_loss_array[@]}; do
 #     for n_layers in ${n_layers_array[@]}; do
 #         for rnn_type in ${rnn_type_array[@]}; do
 #             for axon_delay in ${axon_delay_array[@]}; do
-
 #                 python main.py --n_layers=$n_layers \
 #                                --rnn_type=$rnn_type \
 #                                --axon_delay=$axon_delay \
 #                                --pred_loss=$pred_loss \
 #                                --load_model=1
-
 #             done
 #         done
 #     done
 # done
-
-python main.py --n_layers=4 --rnn_type=hgru --axon_delay=1 --pred_loss=1 --load_model=1
-python main.py --n_layers=4 --rnn_type=hgru --axon_delay=0 --pred_loss=1 --load_model=1
-python main.py --n_layers=4 --rnn_type=lstm --axon_delay=1 --pred_loss=1 --load_model=1
-python main.py --n_layers=4 --rnn_type=conv --axon_delay=1 --pred_loss=1 --load_model=1
-python main.py --n_layers=4 --rnn_type=conv --axon_delay=0 --pred_loss=0 --load_model=1
 
 python main.py --n_layers=3 --rnn_type=hgru --axon_delay=1 --pred_loss=1 --load_model=1
 python main.py --n_layers=3 --rnn_type=hgru --axon_delay=0 --pred_loss=1 --load_model=1
 python main.py --n_layers=3 --rnn_type=conv --axon_delay=1 --pred_loss=1 --load_model=1
 python main.py --n_layers=3 --rnn_type=lstm --axon_delay=1 --pred_loss=1 --load_model=1
 python main.py --n_layers=3 --rnn_type=conv --axon_delay=0 --pred_loss=0 --load_model=1
+
+python main.py --n_layers=4 --rnn_type=hgru --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=4 --rnn_type=hgru --axon_delay=0 --pred_loss=1 --load_model=1
+python main.py --n_layers=4 --rnn_type=lstm --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=4 --rnn_type=conv --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=4 --rnn_type=conv --axon_delay=0 --pred_loss=0 --load_model=1
