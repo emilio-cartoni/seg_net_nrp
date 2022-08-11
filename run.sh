d4017cdd4a58200d60fcaee5b84d5876b9e916e1
albornet <alban.bornet@gmail.ch>
2022-08-11 08:07:10 +0200

    changed training parameters

--- run.sh
+++ run.sh
@@ -20,6 +20,13 @@ python main.py --n_layers=4 --rnn_type=conv --axon_delay=1 --pred_loss=1 --load_
 python main.py --n_layers=4 --rnn_type=conv --axon_delay=0 --pred_loss=0 --load_model=1
 python main.py --n_layers=4 --rnn_type=hgru --axon_delay=1 --pred_loss=0 --load_model=1
 
+python main.py --n_layers=3 --rnn_type=hgru --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=3 --rnn_type=hgru --axon_delay=0 --pred_loss=1 --load_model=1
+python main.py --n_layers=3 --rnn_type=lstm --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=3 --rnn_type=conv --axon_delay=1 --pred_loss=1 --load_model=1
+python main.py --n_layers=3 --rnn_type=conv --axon_delay=0 --pred_loss=0 --load_model=1
+python main.py --n_layers=3 --rnn_type=hgru --axon_delay=1 --pred_loss=0 --load_model=1
+
 # declare -a pred_loss_array=('1' '0')
 # declare -a n_layers_array=('4' '3')
 # declare -a rnn_type_array=('hgru' 'conv' 'lstm')
