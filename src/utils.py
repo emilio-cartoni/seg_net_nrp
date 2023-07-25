b47b3e5e2e7f5638837371c2af21f3963032d9b8
Emilio Cartoni <emilio.cartoni@yahoo.it>
2023-07-25 16:45:00 +0200

    Reduced gap within images in visualization

--- src/utils.py
+++ src/utils.py
@@ -5,11 +5,10 @@ import torch.nn.functional as F
 
 def plot_recons(P_seq, P_seq_true, S_seq, S_seq_true, rect_width=10):
     ''' Plot reconstruction of image and segmentation mask. '''
-    rect_width=320
+    rect_width=5
     N, C, H, W, T = P_seq.shape
     S_seq = onehot_to_rgb(S_seq.numpy())
     S_seq_true = onehot_to_rgb(S_seq_true.numpy())
-    
     h_rect = torch.ones((N, C, rect_width, W, T))
     v_rect = torch.ones((N, C, 2 * H + rect_width, rect_width, T))
     img_data = torch.cat((P_seq_true, h_rect, P_seq), dim=2)
